7089e3a10579b5d2caaeb65690f67dcf6da017a2
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-10-28 18:53:51 +0200

    objects

--- objects/main.js
+++ objects/main.js
@@ -31,7 +31,7 @@ console.log( playlist.songs );
 */
 
 
-
+/*
 
 // **Esercizio 2**
 
@@ -130,8 +130,9 @@ let allUser = (rubrica) => {
 
 allUser(rubrica.users);
 
+*/
 
-
+/*
 
 // **Esercizio 3**
 
@@ -143,12 +144,63 @@ allUser(rubrica.users);
 // Scrivi poi una funzione che stampi in console tutti i modelli presenti nel garage.
 
 
+// garage obj
+const garage = {
+    cars : [
+        {
+            house : "Ford",
+            model : "Mustang Dark horse",
+            hp : "500"
+        },
+        {
+            house : "Nissan",
+            model : "GTR R35 Nismo",
+            hp : "600"
+        },
+        {
+            house : "Porche",
+            model : "911 GT3 RS",
+            hp : "520"
+        },
+        {
+            house : "BMW",
+            model : "M4 Competition",
+            hp : "510"
+        },
+        {
+            house : "Lamborghini",
+            model : "Huracan",
+            hp : "620"
+        }
+        
+    ],
+    carsObj : function (){
+        this.cars.forEach( car => console.log(`House : ${car.house}` + `\n` + `Model : ${car.model}` + `\n` + `Horsepower : ${car.hp}`) );
+    }
+};
+// call of the garage obj methods
+garage.carsObj();
 
+// create a function that stamp all the model of the garage
+let allModelS = (garage) => {
+    garage.cars.forEach(model => {
+        console.log( `All models : ${model.model}` )
+    });
+};
+
+allModelS(garage);
+
+*/
 
 
 // **Esercizio 4**
 
-// Scrivi una funzione che, per il sito www.lamiasuperlibreria.aulab, prenda in input la lista “libri” ed una “categoria” passata dall’utente. Il programma dovrà ritornare una lista di tag anchor (naturalmente questi devono essere delle semplici stringhe da mostrare in console) con il relativo url e come contenuto il titolo del libro. Questa lista dovrà contenere  solo gli elementi appartenenti alla categoria espressa in input.
+// Scrivi una funzione che, per il sito www.lamiasuperlibreria.aulab, prenda in input la lista 
+//“libri” ed una “categoria” passata dall’utente. 
+//Il programma dovrà ritornare una lista di tag anchor 
+// (naturalmente questi devono essere delle semplici stringhe da mostrare in console) 
+// con il relativo url e come contenuto il titolo del libro. 
+// Questa lista dovrà contenere  solo gli elementi appartenenti alla categoria espressa in input.
 
 // La creazione della url dovrà rispettare queste regole:
 
@@ -201,3 +253,55 @@ allUser(rubrica.users);
 // 	'<a href=”www.lamiasuperlibreria.aulab/tolkien/il-signore-degli-anelli.html”>Il signore degli anelli</a>',
 // 	'<a href=”www.lamiasuperlibreria.aulab/herbert/dune.html”>Dune</a>'
 // ]
+
+// array books
+const BOOKS = [
+    {
+        title: "Il Signore degli Anelli",
+        author: "Tolkien",
+        category: "fantasy"
+    },
+    {
+        title: "Harry Potter",
+        author: "Rowling",
+        category: "fantasy",
+    },
+    {
+        title: "Il Conte di Montecristo",
+        author: "Dumas",
+        category: "narrativa classica"
+    },
+    {
+        title: "Dune",
+        author: "Herbert",
+        category: "fantascienza"
+    },
+    {
+        title: "Fight Club",
+        author: "Palahniuk",
+        category: "narrativa moderna"
+    }
+]
+
+// function for user input
+const userInput = ()=> {
+    // this variable contain a propmt for the user input
+    let userInput = prompt(`write the category that you want`);
+    // return the variable userInput
+    return userInput;
+
+};
+// save the function on a variable
+let userCategory = userInput();
+
+// function that find the category
+const findCategory = (userCategory)=> {
+    // this variable contain the filter method 
+    let resultFind = BOOKS.filter(book => book.category.toLocaleLowerCase().includes(userCategory.toLocaleLowerCase()));
+    // foreach
+    resultFind.forEach(book => {
+        console.log(`<a href="www.lamiasuperlibreria.aulab/${book.author.toLowerCase()}/${book.title.toLowerCase().replace(/ /g, "-")}.html">${book.title.toLowerCase().replace(/ /g, "-")}</a>`);
+    });
+}
+
+console.log(findCategory(userCategory));
