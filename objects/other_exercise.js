76bb496c9221b529fcf7b6d9582dc12e6cd265c5
Gellart <gellart0@gmail.com>
2023-10-30 13:25:21 +0100

    obj

--- objects/other_exercise.js
+++ objects/other_exercise.js
@@ -284,3 +284,33 @@ for(const prop in houseForSale){
 };
 
 */
+
+
+
+
+// 1. Create an object called "myConsole"
+    
+//     2. Assign "myConsole" the key "log" with a value that is
+//        an arrow function.
+
+//        This arrow function will have one parameter called "message"
+
+//        The function will just console.log out the "message"
+
+//     3. How would you call this "log" function inside the object?
+
+//     You should be able to do: myConsole.log("Hello, friend!");
+
+//     BONUS: How might the built-in "console" work?
+
+
+// myConsole obj
+const myConsole = {
+
+    log : (message)=> {
+        console.log(message);
+    }
+
+}
+
+myConsole.log("Hello world");
