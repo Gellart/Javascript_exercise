39a314f3d7d01778db85fff2753b6b9501f5fcbf
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-11-02 23:31:38 +0100

    obj

--- objects/other_exercise.js
+++ objects/other_exercise.js
@@ -421,95 +421,73 @@ console.log(eva.addGrade(10));
 
 */
 
-
 /*
 
-// 1. Create a class called "Bookstore"
+// **Esercizio 1**
 
-// 2. The "Bookstore" constructor has a single parameter
-//        called "books" which is an Array of Book Objects.
-//        Assign the argument passed in to the instance within
-//        the constructor.
+// Definisci un oggetto **playlist** che contiene una proprietà **canzoni**. La prorietà canzoni dovrà essere un array.
 
-// 3. Create a "listBooks" method on the "Bookstore" class
-//     that will loop through and print out all the Books
-//         in the Store(assume they have a name and author)
-//        so you can format each like this:
-// `${book.name} by ${book.author}`
+// Inserisci una o 2 canzoni in maniera dinamica e ciascuna deve avere le seguenti proprietà:
 
-// 3. Create another class called "Book"
+// - titolo
+// - nomeCantante
+// - anno
 
-// 4. The "Book" constructor has 2 parameters:
-// - name
-//     - author
-//        Assign these to the instance within the constructor
-
-// 5. Create 2 Books:
-// const nineteen84 = new Book("1984", "George Orwell")
-// const hp = new Book("Harry Potter", "J.K. Rowling")
-
-// 6. Create a Bookstore:
-// const bookstore = new Bookstore([nineteen84, hp])
-
-// 7. Call bookstore.listBooks() and make sure it works:
-// 1984 by George Orwell
-//        Harry Potter by J.K.Rowling
+// Ad ogni canzone inserita stampa in console tutte le canzoni.
 
 
-
-// Bookstore class
-class Bookstore {
-    constructor(books){
-        this.books = books;
-    };
-
-    // listBooks method, that loop through and print out all the Books in the Store
-    listBooks() {
-        for(const prop of this.books){
-            prop.displayBook();
-        }
+// playlist obj
+const playlist = {
+    // obj property
+    songs: [],
+    // create a method that push the songs inside the songs array
+    addSongs(song){
+        this.songs.push(song);
     }
 
-}
+};
 
-// Book class
-class Book {
-    constructor(name, author){
-        this.name = name;
+// constructor song
+class Song {
+    constructor(title, author, year){
+
+        this.title = title;
         this.author = author;
-    }
+        this.year = year;
 
-    displayBook(){
-        console.log(`${this.name} by ${this.author}`);
-    }
-}
+    };
+};
 
-// let's create dome books using the constructor
-let book1 = new Book("1984", "George Orwell");
-let book2 = new Book("Harry Potter", "J.K. Rowling"); 
+let song1 = new Song("past lives", "sapientdream", 2015);
+let song2 = new Song("glimpse of us", "Joji", 2022);
 
-let bookStore = new Bookstore([book1, book2]);
-bookStore.listBooks();
+playlist.addSongs(song1);
+playlist.addSongs(song2);
+
+playlist.songs.forEach(song => {
+    console.log(song.title);
+});
 
 */
+
 
 /*
 
 // **Esercizio 2**
 
-// Definisci un oggetto **rubrica** che ha come proprietà gli **utenti**. Per ogni utente specificare altre proprietà: **nome**, **cognome**, **telefono** e **indirizzo**. Dove indirizzo è a sua volta un altro oggetto contenente altre proprietà (via, città, ecc).
+// Definisci un oggetto **rubrica** che ha come proprietà gli **utenti**. 
+// Per ogni utente specificare altre proprietà: **nome**, **cognome**, **telefono** e **indirizzo**. 
+// Dove indirizzo è a sua volta un altro oggetto contenente altre proprietà (via, città, ecc).
 
 // - Quindi popolare la rubrica con dei dati a piacere.
 // - Crea un metodo all’interno dell’oggetto che restituisca nome cognome e telefono.
 // - Dopo eliminare un elemento e visualizzare nuovamente la rubrica così ottenuta.
 // - Crea poi una funzione che scorra tutti gli utenti all’interno della rubrica.
 
-
-// rubrica obj
+// rubrica obj 
 const rubrica = {
-
-    // property
     users: [
+
         user1 = {
             // property
             name: "gino",
@@ -521,7 +499,7 @@ const rubrica = {
                 streeet: "via roma",
                 number: "44",
                 city: "Turin"
-            },
+            }
         },
 
         user2 = {
@@ -535,7 +513,7 @@ const rubrica = {
                 streeet: "via roma",
                 number: "44",
                 city: "Turin"
-            },
+            }
         },
 
         user3 = {
@@ -549,7 +527,7 @@ const rubrica = {
                 streeet: "via roma",
                 number: "44",
                 city: "Turin"
-            },
+            }
         },
 
         user4 = {
@@ -563,32 +541,36 @@ const rubrica = {
                 streeet: "via roma",
                 number: "44",
                 city: "Turin"
-            },
+            }
         },
     ],
 
-    nameSurname: function() {
+    // return name surname and phone
+    returnUserInfo(){
         this.users.forEach(user => {
-            console.log(`Name:${user.name}` + `\n` + `Surname: ${user.surname}` + `\n` + `Phone: ${user.phone}`)
+            console.log(`Name:${user.name}` + `\n` + `Surname:${user.surname}` + `\n` + `Name:${user.phone}`)
         });
     }
+}
+
+rubrica.returnUserInfo();
+
+// let's remove the last element
+rubrica.users.pop();
+// function that see all the element inside the array
+const checkArray = ()=> {
+    for( const prop in rubrica.users){
+        const value = rubrica.users[prop];
+        console.log(value);
+    };
 };
 
-
-rubrica.nameSurname();
-// let's remove the last elements of the array
-rubrica.users.pop();
-// let's see the obj after pop
-for(const prop in rubrica){
-    const value = rubrica[prop];
-    console.log(value);
-}
+checkArray();
 
 */
 
 
 
-/*
 
 // **Esercizio 3**
 
@@ -599,427 +581,48 @@ for(const prop in rubrica){
 
 // Scrivi poi una funzione che stampi in console tutti i modelli presenti nel garage.
 
+
 // garage obj
 const garage = {
-    cars: [
-
-        {
-            house: "Ford",
-            model: "Mustang Dark horse",
-            hp: "500"
-        },
-        {
-            house: "Nissan",
-            model: "GTR R35 Nismo",
-            hp: "600"
-        },
-        {
-            house: "Porche",
-            model: "911 GT3 RS",
-            hp: "520"
-        },
-        {
-            house: "BMW",
-            model: "M4 Competition",
-            hp: "510"
-        },
-        {
-            house: "Lamborghini",
-            model: "Huracan",
-            hp: "620"
-        }
-
-    ],
-    // method that return house - model and hp
-    returnCarDetails(){
-        for(const car of this.cars){
-            const details = `House: ${car.house}` + `\n` + `${car.model}` + `\n` + `${car.hp}`;
-            console.log(details);
-        }
+    cars: [],
+    // create a method that push the car inside the array
+    addCars(car){
+        this.cars.push(car);
     },
-    // method that return all the car model
-    returnCarModel() {
-        this.cars.forEach(model => {
-            const carModel = model.model;
-            console.log(carModel);
+    returnCarDetails(){
+        this.cars.forEach(car => {
+            console.log(`Brand: ${car.brand}` + `\n` + `Model: ${car.model}` + `\n` + `Hp: ${car.hp}`);
+        });
+    },
+    returnCarBrand(){
+        this.cars.forEach(model=> {
+            console.log(`Model: ${model.model}`);
         });
     }
-}
-
-garage.returnCarDetails();
-garage.returnCarModel();
-
-*/
-
-
-
-/*
-
-// **Esercizio 4**
-
-// Scrivi una funzione che, per il sito www.lamiasuperlibreria.aulab, prenda in input la lista “libri” ed una “categoria” passata dall’utente. Il programma dovrà ritornare una lista di tag anchor (naturalmente questi devono essere delle semplici stringhe da mostrare in console) con il relativo url e come contenuto il titolo del libro. Questa lista dovrà contenere  solo gli elementi appartenenti alla categoria espressa in input.
-
-// La creazione della url dovrà rispettare queste regole:
-
-// - tutte le lettere devono essere minuscole
-// - gli spazi devono essere sostituiti con il simbolo “-”
-// - la prima parte della url sarà il nome dell’autore e l’ultima il nome del libro unito a “.html”
-
-// esempio lista libri:
-
-// const LIBRI = [
-//     {
-//       titolo: "Il Signore degli Anelli",
-//       autore: "Tolkien",
-//       categoria: "fantasy"
-//     },
-//     {
-//       titolo: "Harry Potter",
-//       autore: "Rowling",
-//       categoria: "fantasy",
-//     },
-//     {
-//       titolo: "Il Conte di Montecristo",
-//       autore: "Dumas",
-//       categoria: "narrativa classica"
-//     },
-//     {
-//       titolo: "Dune",
-//       autore: "Herbert",
-//       categoria: "fantascienza"
-//     },
-//     {
-//       titolo: "Fight Club",
-//       autore: "Palahniuk",
-//       categoria: "narrativa moderna"
-//     }
-//   ]
-
-
-//   esempio url:
-
-//   `www.lamiasuperlibreria.aulab/tolkien/il-signore-degli-anelli.html`
-
-//   esempio tag anchor:
-
-//   `<a href=”www.lamiasuperlibreria.aulab/tolkien/il-signore-degli-anelli.html”>Il signore degli anelli</a>`
-
-//   esempio output finale:
-
-// [ 
-// 	'<a href=”www.lamiasuperlibreria.aulab/tolkien/il-signore-degli-anelli.html”>Il signore degli anelli</a>', 
-// 	'<a href=”www.lamiasuperlibreria.aulab/herbert/dune.html”>Dune</a>' 
-// ]
-
-
-// user category
-let userCategory = prompt(`write the category`);
-
-
-// array of obj
-const BOOKS = [
-    {
-        title: "Il Signore degli Anelli",
-        author: "Tolkien",
-        category: "fantasy"
-    },
-    {
-        title: "Harry Potter",
-        author: "Rowling",
-        category: "fantasy",
-    },
-    {
-        title: "Il Conte di Montecristo",
-        author: "Dumas",
-        category: "narrativa classica"
-    },
-    {
-        title: "Dune",
-        author: "Herbert",
-        category: "fantascienza"
-    },
-    {
-        title: "Fight Club",
-        author: "Palahniuk",
-        category: "narrativa moderna"
-    }
-]
-
-
-// function that create the links
-const createLinks = (category, listBooks)=> {
-    // let's create an empty array
-    const result = [];
-    // cycle all the books array
-    listBooks.forEach(book => {
-        // if the user category is the same as books category return something
-        if(book.category.toLowerCase() === category.toLowerCase()){
-            // variable of the books author
-            const bookAuthor = book.author.toLowerCase();
-            // variable for the book title
-            const bookTitle = book.title.toLowerCase().replace(`/\s+g`, `-`);
-            // variable for the url
-            const url = `www.lamiasuperlibreria.aulab/${bookAuthor}/${bookTitle}.html`;
-            // variable for the anchor tag
-            const tagAnchor = `<a href=”${url}”>${book.title}</a>`
-            // let's push the tagAnchor inside the result array
-            result.push(tagAnchor);
-        } else {
-            console.log(`sorry the category was not found`);
-        };
-    });
-
-    return result;
 };
 
+// car constructor
+class Car {
+    constructor(brand, model, hp){
 
-let book1 = createLinks(userCategory,BOOKS);
-console.log(book1);
-
-*/
-
-
-/*
-
-// ** Esercizio 1 **
-
-//     Scrivi una funzione Javascript che stampi in console se un libro è stato letto o meno 
-// controllando la proprietà dell’oggetto`readingStatus`
-
-// const library = [
-//     {
-//         title: 'Bill Gates',
-//         author: 'The Road Ahead',
-//         readingStatus: true
-//     },
-//     {
-//         title: 'Steve Jobs',
-//         author: 'Walter Isaacson',
-//         readingStatus: true
-//     },
-//     {
-//         title: 'Mockingjay: The Final Book of The Hunger Games',
-//         author: 'Suzanne Collins',
-//         readingStatus: false
-//     }
-// ];
-
-// - Se la proprietà è true allora stampare a console:
-// `${titolo del libro} di ${autore del libro } è stato letto!`
-// - Se la proprietà è false allora stampare a console:
-    
-//     `${titolo del libro } di ${autore del libro } non è ancora stato letto!`
-
-
-// library array
-const library = [ 
-    {
-        title: 'Bill Gates',
-        author: 'The Road Ahead',
-        readingStatus: true
-    },
-    {
-        title: 'Steve Jobs',
-        author: 'Walter Isaacson',
-        readingStatus: true
-    },
-    {
-        title: 'Mockingjay: The Final Book of The Hunger Games',
-        author: 'Suzanne Collins',
-        readingStatus: false
-    }
-];
-
-// function that check if the book has been reading or not
-const bookStatus = (library)=> {
-    const bookStatus = [];
-
-    library.forEach(book => {
-        if(book.readingStatus == true){
-            let checkTrue = `${book.title} of ${book.author} has been read`;
-            bookStatus.push(checkTrue);
-        } else {
-            let checkFalse = `${book.title} of ${book.author} hasn't been read`;
-            bookStatus.push(checkFalse);
-        }
-    });
-    return bookStatus;
-}
-
-let result = bookStatus(library);
-console.log(result);
-
-*/
-
-
-/*
-
-// **Esercizio 2**
-
-// Definisci un oggetto che rappresenti una sala da bowling, dovrà contenere 
-// un’array di giocatori che saranno formati dalle proprietà, nome e punteggi.
-
-// Creare una funzione che calcoli il totale di ogni giocatore 
-// e estragga il vincitore della partita.
-
-// const bowling = {
-//     players: [
-//       { name: "Giocatore 1", scores: [10, 7, 9, 7, 6, 10, 10, 9, 10, 8, 7, 10, 10, 10, 9, 8, 10] },
-//       { name: "Giocatore 2", scores: [9, 8, 10, 10, 7, 10, 10, 10, 9, 10, 8, 7, 10, 10, 10, 9, 10] },
-//       { name: "Giocatore 3", scores: [10, 10, 7, 8, 10, 7, 9, 8, 10, 10, 10, 9, 8, 10, 10, 10, 7] }
-//     ]
-//   };
-
-//   // Expected output: Il Vincitore è il Giocatore 2 con 157 punti totali
-
-//   **TIP**: LAVORATE CON LE FUNZIONI.
-
-// Ad es: `checkWinner(bowling)`
-
-// **TIP**: Aggiungete una proprietà `total` per ogni oggetto Player 
-// `{ name: “xxx”, scores: [7, 9, 10], total: 123 }`
-
-const bowling = {
-    players: [
-        { name: "Giocatore 1", scores: [10, 7, 9, 7, 6, 10, 10, 9, 10, 8, 7, 10, 10, 10, 9, 8, 10] },
-        { name: "Giocatore 2", scores: [9, 8, 10, 10, 7, 10, 10, 10, 9, 10, 8, 7, 10, 10, 10, 9, 10] },
-        { name: "Giocatore 3", scores: [10, 10, 7, 8, 10, 7, 9, 8, 10, 10, 10, 9, 8, 10, 10, 10, 7] }
-    ]
-};
-
-// function for calculate the tola score
-const calculateScore = (bowling)=> {
-    // array where that i use to push the score result
-    const scoreResult = [];
-
-    bowling.players.forEach(player => {
-        const sum = player.scores.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        const result = `Name: ${player.name}, Score: ${sum}`;
-        scoreResult.push(result);
-    });
-
-    return scoreResult;  
-};
-
-// function that find the best score
-const bestScore = (bowling)=> {
-    const scores = bowling.players.map(player => player.scores.reduce((accumulator, currentValue)=> accumulator + currentValue,0));
-    const bestScore = Math.max(...scores);
-    const bestPlayer = bowling.players.find(player => player.scores.reduce((accumulator, currentValue)=> accumulator + currentValue,0) === bestScore);
-    console.log(`The best player is: ${bestPlayer.name}, with a score of ${bestScore}`)
-};
-
-let playerScore = calculateScore(bowling);
-console.log(playerScore);
-
-let bestPlayer = bestScore(bowling);
-console.log(bestPlayer);
-
-*/
-
-
-
-// **Esercizio 3**
-
-// Il Calendario Settimanale
-
-// - Scrivi un oggetto contenente i giorni della settimana.
-// - Ogni giorno sarà un array al quale gli si potrà aggiungere un evento.
-
-// L’evento sarà un oggetto contenente le chiavi
-
-// - nomeEvento
-// - inizioEvento (formato “08:00”)
-
-// {
-// 	nomeEvento: "Lezione",
-// 	inizioEvento: "18:30"
-// }
-
-// Il programma dovrà permettere di:
-
-// - aggiungere singoli eventi per un determinato giorno.
-
-// // ricordate che potete accedere alle proprietà di un oggetto:
-// object["key"];
-
-// BONUS: all’inserimento di questi bisognerà ordinare l’array 
-// in modo che l’orario di inizio sia una successione ordinata
-
-/*
-
-Il metodo sort() itera sull'array, passando ogni volta due valori adiacenti alla funzione di confronto. 
-La funzione di confronto valuta quindi la differenza tra il primo valore e il secondo. Questo determinerà se la funzione 
-restituisce un valore positivo, negativo o neutro.
-Se il valore restituito è positivo, significa che il primo valore è maggiore del secondo. 
-Ad esempio, se confronti 7 e 4, ottieni 3. 
-Pertanto, il primo valore viene posizionato dopo il secondo nell'ordine crescente.
-Se il valore restituito è negativo, significa che il primo valore è minore del secondo. 
-Ad esempio, se confronti 2 e 7, ottieni -5. 
-Pertanto, il primo valore viene posizionato prima del secondo nell'ordine crescente.
-Un valore neutro, o zero, indica che i due valori sono uguali, quindi l'ordine non ha bisogno di essere modificato.
-
-*/
-
-// .sort(function(a, b) {
-//     return a.inizioEvento - b.inizioEvento;
-// });
-
-// - filtrare gli eventi di una specifica giornata.
-// - cercare gli eventi per nome: questa funzione mi ritornerà le eventuali occorrenze, 
-// dell’evento, divise per giornata.
-
-// // ricordate che potete ciclare un oggetto
-// for(let key in object) {
-// 	// ...
-// }
-
-// // potrebbe tornarvi utile
-// .filter(evento => {
-// 	// ...
-// });
-
-// TIP: lavorate con le funzioni, per esempio per aggiungere un evento fate una funzione 
-// aggiungiEvento() o addEvent()
-
-// days of the week obj
-const week = {
-
-    Monday : [],
-    Tuesday : [],
-    Wednesday : [],
-    Thursday : [],
-    Friday : [],
-    Saturday : [],
-    Sunday : []
-
-};
-
-// contructor for create the vent
-class Event {
-    constructor(nameEvent, startEvent){
-
-        this.nameEvent = nameEvent;
-        this.startEvent = startEvent;
+        this.brand = brand;
+        this.model = model;
+        this.hp = hp;
 
     };
-    // add event mehod
-    addEvent(day){
-        if(week.hasOwnProperty(day)){
-            week[day].push(this);
-            console.log(`${this.nameEvent} has been added to ${day}`);
-        } else {
-            console.log(`please try again`)
-        }
-    }
 };
 
-// let's create the events
-let event1 = new Event("lesson","18.30");
-event1.addEvent("Monday");
+let car1 = new Car("Ford", "Mustang Dark horse", 500);
+let car2 = new Car("Nissan", "GTR R35 Nismo", 600);
+let car3 = new Car("Porche", "911 GT3 RS", 520);
+let car4 = new Car("BMW", "M4 Competition", 510);
+let car5 = new Car("Lamborghini", "Huracan", 620);
 
-// let's see if there are some events
-for(const prop in week){
-    const value = week[prop];
-    console.log(value);
-};
+garage.addCars(car1);
+garage.addCars(car2);
+garage.addCars(car3);
+garage.addCars(car4);
+garage.addCars(car5);
+
+garage.returnCarDetails();
+garage.returnCarBrand();
