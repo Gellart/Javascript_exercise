39d75e9d73287dac85223039af504cd05cd74502
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-11-09 23:48:23 +0100

    objs & array-methods

--- array/methods.js
+++ array/methods.js
@@ -21,27 +21,45 @@ console.log(calculateSum(numbers));
 
 */
 
+/*
 
 // Exercise 2: Product of Array Elements
 // Calculate the product of all elements in an array.
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const calculateProduct = (array)=> {
+    const product = array.reduce((accumulator, currentValue)=> {
+        return accumulator += currentValue; 
+    },0);
+    return product;
+};
 
+console.log(calculateProduct(numbers));
 
+*/
+
+/*
 // Exercise 3: Flatten Nested Arrays
 // Flatten an array of nested arrays into a single-dimensional array.
 
-// Exercise 4: Count Occurrences
-// Count the occurrences of each element in an array and return an object with element counts.
+const cars = [
+    ["Toyota", "Camry", 2019],
+    ["Honda", "Civic", 2020],
+    ["Ford", "Mustang", 2018],
+    ["Chevrolet", "Impala", 2017]
+];
 
-// Exercise 5: Concatenate Strings
-// Concatenate an array of strings into a single string.
+const flattTheArray = (array)=> {
+    const flatten = array.reduce((accumulator, currentValue)=> {
+        return accumulator.concat(currentValue);
+    },[]);
+    return flatten;
+};
 
-// Exercise 6: Average of Array Elements
-// Calculate the average of all elements in an array.
+console.log(flattTheArray(cars));
 
-// Exercise 7: Maximum Num
-
+*/
 
 
 
@@ -50,14 +68,23 @@ console.log(calculateSum(numbers));
 // ### Challenge 1: Flatten Nested Arrays
 // Given an array of nested arrays, use `reduce()` to flatten the array into a single-dimensional array.
 
-const nestedArray = [[1, 2], [3, 4, [5, 6]], [7, 8]];
-const normalArray = nestedArray.reduce((accumulator, currentValue)=> {
-    return accumulator.concat(currentValue);
-}, []);
+const nestedArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
 
-console.log(normalArray);
+const singleDimensionalArray = (array)=> {
+    const singleArray = array.reduce((accumulator, currentvalue)=> {
+        return accumulator.concat(currentvalue);
+    },[]);
+    return singleArray;
+};
+
+console.log(singleDimensionalArray(nestedArray));
 
 */
+
 
 /*
 
@@ -65,28 +92,61 @@ console.log(normalArray);
 // Given a number, calculate its factorial using `reduce()`. 
 // The factorial of a number is the product of all positive integers from 1 to the number itself.
 
-const numbers = [1, 2, 3, 5, 6, 7, 8, 9, 10];
+const numbers = [10, 77, 46, 25, 48, 6, 94, 13, 17, 58];
 
-const calculateFactorial = (numbers)=> {
+const calculateFactorial = (array)=> {
 
-    const factorialNumber = numbers.reduce((accumulator, currentValue)=> {
-        return accumulator * currentValue;
-    }, 1);
-    return factorialNumber;
+    const evenNumbers = array.filter((number)=> number % 2 === 0);
+    console.log(evenNumbers);
+    const factorial = evenNumbers.reduce((accumulator, currentValue)=> {
+        return accumulator += currentValue;
+    }, 0);
+
+    return factorial;
 };
 
-// Example usage
-numbers.forEach(number => {
-    const factorial = calculateFactorial(number);
-    console.log(`Factorial of ${number} is: ${factorial}`);
-});
+console.log(calculateFactorial(numbers));
 
 */
+
+
 
 
 // ### Challenge 3: Grouping Objects by Property
 // Given an array of objects, group the objects by a specific property using `reduce()`. 
 // For example, group an array of objects by their age property.
+
+const cars = [
+    { make: "Toyota", model: "Camry", year: 2019 },
+    { make: "Honda", model: "Civic", year: 2020 },
+    { make: "Ford", model: "Mustang", year: 2018 },
+    { make: "Chevrolet", model: "Impala", year: 2017 }
+];
+
+const groupByProperties = (array) => {
+    const grouped = array.reduce((accumulator, currentValue) => {
+        const make = currentValue.make;
+        const model = currentValue.model;
+        const year = currentValue.year;
+
+        if (!accumulator[make]) {
+            accumulator[make] = {};
+        }
+        if (!accumulator[make][model]) {
+            accumulator[make][model] = {};
+        }
+        if (!accumulator[make][model][year]) {
+            accumulator[make][model][year] = [];
+        }
+
+        accumulator[make][model][year].push(currentValue);
+        return accumulator;
+    }, {});
+
+    return grouped;
+};
+
+console.log(groupByProperties(cars));
 
 
 
@@ -94,11 +154,14 @@ numbers.forEach(number => {
 // ### Challenge 4: Calculate Average Grade
 // Given an array of objects representing students with grades, calculate the average grade using `reduce()`.
 
+
+
+
 // ### Challenge 5: Find Longest Word
 // Given an array of words, find the longest word using `reduce()`.
 
 // ### Challenge 6: Balance Parentheses
-// Given a string of parentheses (e.g., `"((())())()"`), use `reduce()` to determine if the parentheses are balanced. 
+// Given a string of parentheses (e.g., `"((())())()"`), use `reduce()` to determine if the parentheses are balanced.
 // A string of parentheses is balanced if every opening parenthesis has a corresponding closing parenthesis.
 
 // ### Challenge 7: Remove Duplicate Characters
